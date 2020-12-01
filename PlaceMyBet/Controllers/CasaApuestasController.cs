5f095a26833702c3fa4dc32175eef29eba1ea848
Alex Montalvo <almosa01@floridauniversitaria.es>
2020-12-01 18:17:31 +0100

    .

--- PlaceMyBet/Controllers/CasaApuestasController.cs
+++ PlaceMyBet/Controllers/CasaApuestasController.cs
@@ -19,6 +19,7 @@ namespace PlaceMyBet.Controllers
         // GET: api/CasaApuestas/5
         public CasaApuestas Get(int id)
         {
+            //var
             var repo = new CasaApuestasRepository();
             CasaApuestas c = repo.Retrieve();
             return c;
